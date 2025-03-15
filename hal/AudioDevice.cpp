034945360d24c6cea66781b829934aeef5cca4b4
bo.yu <bo.yu@tcl.com>
2025-03-15 12:22:30 +0800

    [FPS-1221] 免提通话和playback播放两个场景需要区分AW MEC模块 [3/3]　ahal part
    * Description
      New design for AW SKTune 免提下行和playback播放场景区分MEC保护模块
      Update ahal code
    * Solution
      AS Task requirement
    * Test Steps
      Follow description
    * Test Result
      pass
    
    
    Change-Id: I9815ec03fdb86f308b5cf0a09b1dc089706e3b84

--- hal/AudioDevice.cpp
+++ hal/AudioDevice.cpp
@@ -1433,6 +1433,7 @@ int AudioDevice::SetMode(const audio_mode_t mode) {
 
     AHAL_DBG("enter: mode: %d", mode);
     ret = voice_->SetMode(mode);
+    pal_set_audio_mode(mode);
     AHAL_DBG("Exit ret: %d", ret);
     return ret;
 }
